b7d817d4cf22769e4a3f86f0e2af88b884a7243c
Abhijeet Kamble <abhijeetk597@gmail.com>
2024-11-02 07:04:55 +0530

    module and common js

---
+++ jsfoundation/part9_adv/mathOperationsC.js
@@ -0,0 +1,20 @@
+// common js
+// named export
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+};
